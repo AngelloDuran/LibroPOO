808bdb9bf72faf725da3ec92d00c433c18faf931
AngelloDuran <duea020109hdfrsna5@gmail.com>
2024-04-30 02:02:19 -0600

    LibroPrivate

--- Libro.java
+++ Libro.java
@@ -1,8 +1,8 @@
 public class Libro {
-    String isbn;
-    String titulo;
-    String autor;
-    int numPaginas;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int numpaginas;
 
     public void mostrarIsbn() {
         System.out.println("Este es el ISBN del libro:  " + isbn);
@@ -17,7 +17,44 @@ public class Libro {
     }
 
     public void mostrarNumPaginas() {
-        System.out.println("Este es el numero de pagina del libro: " + numPaginas);
+        System.out.println("Este es el numero de pagina del libro: " + numpaginas);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumPaginas() {
+        return numpaginas;
+    }
+
+    public void setNumPaginas(int numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro [isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", numPaginas=" + numpaginas + "]";
     }
 
 }
--- MainLibro.java
+++ MainLibro.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class MainLibro {
-
     public static void main(String[] args) {
-        Scanner p1 = new Scanner(System.in);
-        System.out.println("Ingresa el ISBN del libro: ");
-        String isbn = p1.nextLine();
-        System.out.println("Ingresa el Titulo del libro: ");
-        String tittle = p1.nextLine();
-        System.out.println("Ingresa el autor del libro: ");
-        String autor = p1.nextLine();
-        System.out.println("Ingresa el Numero de pagina del libro: ");
-        int numPaginas = p1.nextInt();
-        Libro p2 = new Libro();
-        p2.isbn = isbn;
-        p2.titulo = tittle;
-        p2.autor = autor;
-        p2.numPaginas = numPaginas;
-        p2.mostrarIsbn();
-        p2.mostrarTitulo();
-        p2.mostrarAutor();
-        p2.mostrarNumPaginas();
+
+        {
+            Scanner p1 = new Scanner(System.in);
+            System.out.println("Ingresa el ISBN del libro: ");
+            String isbn = p1.nextLine();
+            System.out.println("Ingresa el Titulo del libro: ");
+            String titulo = p1.nextLine(); // Se corrigió el nombre de la variable "tittle" a "titulo"
+            System.out.println("Ingresa el autor del libro: ");
+            String autor = p1.nextLine();
+            System.out.println("Ingresa el Numero de pagina del libro: ");
+            int numPaginas = p1.nextInt();
+            p1.nextLine(); // Se agrega una llamada a nextLine() para consumir el salto de línea pendiente
+            Libro p2 = new Libro();
+            p2.setIsbn(isbn);
+            p2.setTitulo(titulo);
+            p2.setAutor(autor);
+            p2.setNumPaginas(numPaginas);
+            p2.mostrarIsbn();
+            p2.mostrarTitulo();
+            p2.mostrarAutor();
+            p2.mostrarNumPaginas();
+            System.out.println(p2.toString());
+
+        }
     }
 }
